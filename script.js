c3fb95e8a1b80609aa580d5f50a1161002682780
MASOKI886 <kc86kim@gmail.com>
2025-05-27 13:43:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -452,3 +452,25 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("contextmenu", function (e) {
   e.preventDefault();
 });
+
+document.getElementById("fullscreen-btn").addEventListener("click", () => {
+  const elem = document.documentElement;
+  if (!document.fullscreenElement) {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { // Safari
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { // IE11
+      elem.msRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+  }
+});
+
